5c0af15286e98b5f2101b76fbb77c58cea478f59
nato4ka1987 <92274011+nato4ka1987@users.noreply.github.com>
2021-11-09 23:14:12 +0300

    Add files via upload

---
+++ part2.py
@@ -0,0 +1,24 @@
+'''
+Создайте собственный класс-исключение, обрабатывающий ситуацию деления на нуль.
+Проверьте его работу на данных, вводимых пользователем. При вводе пользователем
+нуля в качестве делителя программа должна корректно обработать эту ситуацию и
+не завершиться с ошибкой.
+'''
+
+class Exeption:
+    def __init__(self, divider, denominator):
+        self.divider = divider
+        self.denominator = denominator
+
+    @staticmethod
+    def divide_by_null(divider, denominator):
+        try:
+            return (divider / denominator)
+        except:
+            return (f"Деление на ноль недопустимо")
+
+
+div = Exeption(10, 200)
+print(Exeption.divide_by_null(10, 0))
+print(Exeption.divide_by_null(10, 0.5))
+print(div.divide_by_null(200, 0))
